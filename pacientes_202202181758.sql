f62cd65e917b864351edf946f60e09283d4499b0
Wesly <weslyramalho@yahoo.com.br>
2022-02-18 17:59:06 -0300

    pacientes

---
+++ pacientes_202202181758.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.pacientes (id,nome,data_de_nascimento,telefone) VALUES
+	 (1,'Joao','2015-05-31','94991667554'),
+	 (2,'Otavio','2006-10-21','94991667454'),
+	 (3,'Andre','2000-10-21','94997767454'),
+	 (4,'Raimundo','2001-10-21','94991557454');
