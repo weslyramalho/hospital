06769a522e5c169b0183e1a7ebc24e6e94b1d38e
Wesly <weslyramalho@yahoo.com.br>
2022-02-18 17:57:30 -0300

    historico_do_paciente

---
+++ historico_do_paciente_202202181756.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.historico_do_paciente (id,data_e_hora_de_entrada,data_e_hora_de_saida,diagnostico,id_pacientes,id_funcionarios) VALUES
+	 (1,'2022-01-02 04:05:06','2022-01-03 04:05:06','covid-19',1,1),
+	 (2,'2022-01-02 04:05:06','2022-01-03 04:05:06','covid-19',2,2),
+	 (3,'2022-01-02 04:05:06','2022-01-03 04:05:06','covid-19',3,4);
